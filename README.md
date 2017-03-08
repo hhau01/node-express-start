dfc42eb873c64dbbed27e0b3940d9f26ac6152a9
Henry Au <henryau01@gmail.com>
2017-03-08 00:10:40 -0800

    install dev dependencies and set up common webpack config

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Node Express Ez
 
-sudo npm install express-generator -g
+npm install express-generator -g
 
 express . --hbs
 
 npm install
 
+## Project Dependencies
+
 npm install --save zone.js rxjs core-js
 
 npm install --save-dev @types/node @types/core-js
+
+## Development Dependencies
+
+<!-- or official just not 1.0 -->
+npm install --save-dev webpack@2.1.0-beta.21 webpack-merge angular2-template-loader awesome-typescript-loader del-cli html-loader typescript angular2-router-loader raw-loader
---
+++ webpack.config.common.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'app': './assets/app/main.ts'
+    },
+
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    'awesome-typescript-loader', 
+                    'angular2-template-loader',
+                    'angular2-router-loader'
+                ]
+            },
+            // Webpack Beta 26 or higher
+            // {
+            //     test: /\.html$/,
+            //     loader: 'html-loader'
+            // },
+            // {
+            //     test: /.css/,
+            //     loader: 'raw-loader'
+            // }
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /.css/,
+                loader: 'raw'
+            }
+        ]
+    }
+};
